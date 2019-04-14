c22f00a8f34d539536b343465b98a8fa23d20040
Aditi Thuse <aditi.thuse@gmail.com>
2019-04-14 12:28:24 -0700

    Added Basket Model

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -8,14 +8,16 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.SQL
 {
-    public class DataContext: DbContext 
+    public class DataContext : DbContext
     {
-        public DataContext():base("DefaultConnection")
+        public DataContext() : base("DefaultConnection")
         {
 
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets {get;set;}
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
